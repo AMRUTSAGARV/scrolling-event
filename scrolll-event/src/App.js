245ac6799c2dc3e5b432bf8cb625388bc7d29532
Amrut Sagar <amrutsagar06@gmail.com>
2022-09-07 09:58:27 +0530

    scroll event implemented

--- scrolll-event/src/App.js
+++ scrolll-event/src/App.js
@@ -3,18 +3,37 @@ import axios from "axios";
 import "./App.css";
 
 const App = () => {
+  let offset = 0;
   const [list, setList] = useState([]);
 
   const loadMoreList = () => {
-    axios.get("https://dummyjson.com/products").then(({ data }) => {
-      const newList = [];
-      data.products.forEach((l) => newList.push(l.thumbnail));
-      setList(newList);
-    });
+    axios
+      .get(`https://dummyjson.com/products?skip=5&limit=10&offset=${offset}`)
+      .then(({ data }) => {
+        const newList = [];
+        data.products.forEach((l) => newList.push(l.thumbnail));
+        // data.products.forEach((l) => newList.push(l.title));
+        setList((oldList) => [...oldList, ...newList]);
+      });
+    offset += 10;
+  };
+
+  const handleScroll = (e) => {
+    // console.log("top: ", e.target.documentElement.scrollTop);
+    // console.log("win: ", window.innerHeight);
+    // console.log("height: ", e.target.documentElement.scrollHeight);
+    if (
+      window.innerHeight + e.target.documentElement.scrollTop + 1 >=
+      e.target.documentElement.scrollHeight
+    ) {
+      // console.log("at the bottom of the page!");
+      loadMoreList();
+    }
   };
 
   useEffect(() => {
     loadMoreList();
+    window.addEventListener("scroll", handleScroll);
   }, []);
 
   return (
@@ -24,12 +43,12 @@ const App = () => {
           return (
             <div className="column">
               <div className="row">
-                <img className="img" src={l} />
+                <img alt="" className="img" src={l} />
               </div>
               <div className="ide" key={i}>
                 {i + 1}
               </div>
-              <div className="title">OnePlus 5T</div>
+              <div></div>
             </div>
           );
         })}
